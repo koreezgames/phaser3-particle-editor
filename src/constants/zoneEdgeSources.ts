c14e1836331b976fb26a9105bb78ba396f41c864
Sargis Sargsyan <sargsyan.sargis.89@gmail.com>
2018-09-04 15:33:51 +0400

    bug fixings

--- src/constants/zoneEdgeSources.ts
+++ src/constants/zoneEdgeSources.ts
@@ -5,16 +5,16 @@ const zoneEdgeSources = [
       x: 0,
       y: 0,
       width: 0,
-      height: 0
-    }
+      height: 0,
+    },
   },
   {
     shapeType: 'Circle',
     source: {
       x: 0,
       y: 0,
-      radius: 0
-    }
+      radius: 0,
+    },
   },
   {
     shapeType: 'Ellipse',
@@ -22,29 +22,29 @@ const zoneEdgeSources = [
       x: 0,
       y: 0,
       width: 0,
-      height: 0
-    }
+      height: 0,
+    },
   },
-  {
-    shapeType: 'Line',
-    source: {
-      x1: 0,
-      y1: 0,
-      x2: 0,
-      y2: 0
-    }
-  },
-  {
-    shapeType: 'Triangle',
-    source: {
-      x1: 0,
-      y1: 0,
-      x2: 0,
-      y2: 0,
-      x3: 0,
-      y3: 0
-    }
-  },
+  // {
+  //   shapeType: 'Line',
+  //   source: {
+  //     x1: 0,
+  //     y1: 0,
+  //     x2: 0,
+  //     y2: 0
+  //   }
+  // },
+  // {
+  //   shapeType: 'Triangle',
+  //   source: {
+  //     x1: 0,
+  //     y1: 0,
+  //     x2: 0,
+  //     y2: 0,
+  //     x3: 0,
+  //     y3: 0
+  //   }
+  // },
 ];
 
 export default zoneEdgeSources;
