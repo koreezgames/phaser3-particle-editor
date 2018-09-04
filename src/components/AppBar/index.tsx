9020a8de5abc3b972b45bbd150095d036d1cb570
Artur Vardanyan <a.vardanyan1997@gmail.com>
2018-09-04 19:23:29 +0400

    new AppBarMenu component

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -9,6 +9,7 @@ import {
   WithStyles,
 } from '@material-ui/core';
 import packageJSON from '../../../package.json';
+import AppBarMenu from '../AppBarMenu';
 
 const styles = {
   flex: {
@@ -25,6 +26,7 @@ class AppBar extends Component<EditorStoreProp & WithStyles<typeof styles>> {
     return (
       <MaterialAppBar position="sticky">
         <Toolbar variant="dense">
+          <AppBarMenu />
           <Typography variant="title" color="inherit" className={classes.flex}>
             {name.value}
           </Typography>
