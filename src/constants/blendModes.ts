bcd033381c35e61360e61b1a7256a21a089bcbb0
Sargis Sargsyan <sargsyan.sargis.89@gmail.com>
2018-10-12 10:54:24 +0400

    added missing blend mode 'SCREEN'

--- src/constants/blendModes.ts
+++ src/constants/blendModes.ts
@@ -2,6 +2,7 @@ const blendModes = [
   { text: 'NORMAL', value: 0 },
   { text: 'ADD', value: 1 },
   { text: 'MULTIPLY', value: 2 },
+  { text: 'SCREEN', value: 3 },
 ];
 
 export default blendModes;
